9737e1011a92d1951846bd478a1b6c92f450cc17
thanhchi274 <anarapham274@gmail.com>
2021-08-14 09:55:21 +0700

    first commint

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { google } = require("googleapis");
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.get("/", async (req, res) => {
+ const auth = new google.auth.GoogleAuth({
+  keyFile: "credentials.json",
+  scopes: "https://www.googleapis.com/auth/spreadsheets",
+ });
+ // Create client instance for auth
+ const client = await auth.getClient();
+ // Instance of Google Sheets API
+ const googleSheets = google.sheets({ version: "v4", auth: client });
+ const spreadsheetId = req.query.id;
+ // Get metadata about spreadsheet
+ const metaData = await googleSheets.spreadsheets.get({
+  auth,
+  spreadsheetId,
+ });
+ // Read rows from spreadsheet
+ const getRows = await googleSheets.spreadsheets.values.get({
+  auth,
+  spreadsheetId,
+  range: req.query.sheets,
+ });
+
+ res.status(200).send(getRows.data);
+});
+app.listen(5000, (req, res) => console.log("running on 5000"));
