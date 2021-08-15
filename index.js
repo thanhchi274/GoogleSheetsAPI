21502d785a75b8b0395a2a17f9bc5cf94ee38f14
thanhchi274 <anarapham274@gmail.com>
2021-08-15 15:38:26 +0700

    update routes

--- index.js
+++ index.js
@@ -7,12 +7,11 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+const auth = new google.auth.GoogleAuth({
+ keyFile: "credentials.json",
+ scopes: "https://www.googleapis.com/auth/spreadsheets",
+});
 app.get("/", async (req, res) => {
- const auth = new google.auth.GoogleAuth({
-  keyFile: "credentials.json",
-  scopes: "https://www.googleapis.com/auth/spreadsheets",
- });
  // Create client instance for auth
  const client = await auth.getClient();
  // Instance of Google Sheets API
@@ -23,16 +22,24 @@ app.get("/", async (req, res) => {
   auth,
   spreadsheetId,
  });
+ res.status(200).send(metaData.data.sheets);
+});
+app.get("/sheets", async (req, res) => {
+ const spreadsheetId = req.query.id;
+ // Create client instance for auth
+ const client = await auth.getClient();
+ // Instance of Google Sheets API
+ const googleSheets = google.sheets({ version: "v4", auth: client });
  // Read rows from spreadsheet
  const getRows = await googleSheets.spreadsheets.values.get({
   auth,
   spreadsheetId,
   range: req.query.sheets,
  });
-
  res.status(200).send(getRows.data);
 });
-app.listen(process.env.PORT || 3000, function () {
+
+app.listen(process.env.PORT || 5000, function () {
  console.log(
   "Express server listening on port %d in %s mode",
   this.address().port,
